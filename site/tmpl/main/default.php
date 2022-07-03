9a13f2a69b77d5159d8094edbf8eb20643e07de3
Luca Neukom <luca.neukom@gmail.com>
2022-07-03 22:20:43 +0200

    add twig and interpret template

--- site/tmpl/main/default.php
+++ site/tmpl/main/default.php
@@ -2,7 +2,19 @@
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted Access');
+
+$root = dirname(dirname(dirname(__FILE__)));
+require_once($root.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php');
+
+$loader = new \Twig\Loader\ArrayLoader([
+    'index' => 'Hello {{ name }}!',
+    'template' => $this->getModel()->getItem()->template,
+]);
+$twig = new \Twig\Environment($loader);
+$result = $twig->render('template', ['message' => $this->getModel()->getItem()->message]);
+
 ?>
 <h2>Hello world!</h2>
-<p><?= $this->getModel()->getItem()->message; ?></p>
-<p><?= $this->getModel()->getItem()->template; ?></p>
+<?php 
+    echo $result;
+?>
